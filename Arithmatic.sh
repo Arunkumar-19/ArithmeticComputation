d265f118ed5f6af7cde5282a36158bf949e30033
“Arunkumar19” <arunnissy@gmail.com>
2020-06-24 00:58:01 +0530

    UC-6:added

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -4,7 +4,10 @@ read -p "Enter a : " a
 read -p "Enter b : " b
 read -p "Enter c : " c
 
+declare -A Arithmetic
+Arithmetic[v]=$((a+b*c))
+Arithmetic[w]=$((a*b+c))
+Arithmetic[x]=`echo $a $b $c | awk '{print $3+$1/$2}'`
+Arithmetic[y]=`echo $a $b $c | awk '{print $3%$1+$2}'`
 
-
-y=`echo $a $b $c | awk '{print $3%$1+$2}'`
-echo $y
+echo "Results for evrey computation " ${Arithmetic[@]}
