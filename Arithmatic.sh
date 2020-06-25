f43bc3e7dcaa26b16d800c639cb7af8ad8cc6028
“Arunkumar19” <arunnissy@gmail.com>
2020-06-25 23:45:53 +0530

    UC-8:added

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -11,3 +11,23 @@ Arithmetic[x]=`echo $a $b $c | awk '{print $3+$1/$2}'`
 Arithmetic[y]=`echo $a $b $c | awk '{print $3%$1+$2}'`
 
 echo "Results for evrey computation " ${Arithmetic[@]}
+
+echo "Array in original order"
+echo ${Arithmetic[@]}
+
+for ((i = 0; i<5; i++))
+do
+
+    for((j = 0; j<5-i-1; j++))
+    do
+
+        if [ ${Arithmetic[j]} -gt ${Arithmetic[$((j+1))]} ]
+        then
+            temp=${Arithmetic[j]}
+            Arithmetic[$j]=${Arithmetic[$((j+1))]}
+            Arithmetic[$((j+1))]=$temp
+        fi
+    done
+done
+echo "Array in sorted order :"
+echo ${Arithmetic[@]} | sort -nr
